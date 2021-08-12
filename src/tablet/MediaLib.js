1ec553e19779452fffcea530df8b1ea7a1a1877a
Chris Garrity <chrisg@media.mit.edu>
2021-08-12 16:08:17 -0400

    Handle new people sprites
    * change the palette in the Paint editor to have more skin tones (6)
    * Add new assets to the localizations for later translation
    * Add handling for a new section of the Media json for legacy sprites.
    
    Existing projects may use current library sprites, so they need to be in the app media keys even if they are no longer displayed in the library by default.

--- src/tablet/MediaLib.js
+++ src/tablet/MediaLib.js
@@ -5,6 +5,7 @@ let path;
 let samples;
 let backgrounds;
 let sprites;
+let legacySprites;
 let sounds;
 let keys = {};
 
@@ -39,6 +40,7 @@ export default class MediaLib {
             path = parsedResult.path;
             samples = parsedResult.samples;
             sprites = parsedResult.sprites;
+            legacySprites = parsedResult.legacySprites;
             backgrounds = parsedResult.backgrounds;
             sounds = parsedResult.sounds;
 
@@ -59,6 +61,11 @@ export default class MediaLib {
         for (let i = 0; i < backgrounds.length; i++) {
             backgrounds[i].name = Localization.localize('BACKGROUND_' + backgrounds[i].md5);
         }
+
+        // Localize names of legacy sprites
+        for (let i = 0; i < legacySprites.length; i++) {
+            legacySprites[i].name = Localization.localize('CHARACTER_' + legacySprites[i].md5);
+        }
     }
 
     static generateKeys () {
@@ -70,6 +77,14 @@ export default class MediaLib {
         for (let i = 0; i < sprites.length; i++) {
             var spr = sprites[i];
             keys[spr.md5] = {width: spr.width, height: spr.height, name: spr.name};
+        }
+
+        // when we change sprites (or remove them) the old ones still need to be in keys
+        // for projects that were created before the change
+        console.log('add legacy sprites to the keys');
+        for (let i = 0; i < legacySprites.length; i++) {
+            var legacySpr = legacySprites[i];
+            keys[legacySpr.md5] = {width: legacySpr.width, height: legacySpr.height, name: legacySpr.name};
         }
     }
 }
